98815f489dc96af56bd132244362804a9be659c9
rpietkiewicz <35366815+rpietkiewicz@users.noreply.github.com>
2021-08-15 21:13:52 +1000

    Add classes, console messages and some standard small config changes

--- Airplane/Airplane/Program.cs
+++ Airplane/Airplane/Program.cs
@@ -6,7 +6,24 @@ namespace Airplane
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("*****Welcome to Airplane*****\n Press any key to continue.");
+            Console.ReadLine();
+            SevenFourSeven fourhundred = new SevenFourSeven();
+            fourhundred.airline = "Lufthansa";
+            fourhundred.manufacturer = "Boeing";
+            fourhundred.flightNumber = 7540;
+            Console.WriteLine(fourhundred.flightNumber);
+            //Console.WriteLine(fourhundred.flightNumber);
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+
+            AThree twenty = new AThree();
+            twenty.airline = "Air France";
+            twenty.manufacturer = "Airbus";
+            twenty.flightNumber = 1145;
+            Console.WriteLine(twenty.flightNumber);
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
