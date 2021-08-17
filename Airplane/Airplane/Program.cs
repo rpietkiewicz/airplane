71a3ad2ac4c31e66c8777600f9b288ec3dab1934
rpietkiewicz <35366815+rpietkiewicz@users.noreply.github.com>
2021-08-17 16:49:22 +1000

    Test the switch statement and make some small config changes

--- Airplane/Airplane/Program.cs
+++ Airplane/Airplane/Program.cs
@@ -6,6 +6,7 @@ namespace Airplane
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("*****Welcome to Airplane*****\n Press any key to continue.");
             Console.ReadLine();
             Console.WriteLine("%%%%%Airplane is program where you can%%%%%\n%%%%%view a variety of information for%%%%%\n%%%%%popular airplanes. Press any key to continue%%%%%");
@@ -41,7 +42,44 @@ namespace Airplane
             eight.flightNumber = 1946;
             Console.WriteLine(eight.flightNumber);
             //Console.WriteLine("Hello World!");
+            */
+
+            Console.WriteLine(GetFlightNumber(0));
             Console.ReadLine();
+        }
+
+        static string GetFlightNumber(int theAirline)
+        {
+            string numberFlight;
+
+            switch (theAirline)
+            {
+                case 0: //Lufthansa
+                    numberFlight = "7540";
+                    
+                    break;
+                case 1: //Air France
+                    numberFlight = "1145";
+
+                    break;
+                case 2: //Austrian Airlines
+                    numberFlight = "1890";
+
+                    break;
+                case 3: //American
+                    numberFlight = "1946";
+
+                    break;
+                default:
+                    numberFlight = "Invalid airline";
+                    break;
+            }
+
+            return numberFlight;
         }
     }
 }
+//implement case rather than if statement
+//user enter Lufthansa-> return 7540
+//Air France->1145
+//n->n
